c95bf3adc405700bf354d1d522585b016147a65b
Alex Mizuki <alex.mizuki@gmail.com>
2013-07-27 22:47:56 -0400

    update md 4.1.8 and mono 3.1.2

--- __internal_build.sh
+++ __internal_build.sh
@@ -3,10 +3,10 @@ export DEBIAN_FRONTEND=noninteractive
 # debian/ubuntu packages to install for installation from source
 sudo apt-get -q -y install build-essential autoconf libtool mono-gmcs libglib2.0-dev libpango1.0-dev libatk1.0-dev libgtk2.0-dev libglade2-dev libart-2.0-dev libgnomevfs2-dev libgnome2-dev libgnomecanvas2-dev libgnomeui-dev libmono-addins-cil-dev libmono-addins-gui-cil-dev wget unzip
 
-XS_VERSION=4.0.9
-XS_TAG=monodevelop-4.0.9
-MONO_VERSION=3.0.12
-MONO_TAG=mono-3.0.12
+XS_VERSION=4.1.8
+XS_TAG=monodevelop-4.1.8
+MONO_VERSION=3.1.2
+MONO_TAG=mono-3.1.2
 GTK_SHARP_TAG=gtk-sharp-2-12-branch
 GNOME_SHARP_TAG=master
 FSHARP_TAG=3.0.27
